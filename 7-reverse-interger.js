9142f42541bc78ff96f194bef214264c62029146
Rob Fagg <razorfagg@gmail.com>
2021-09-13 22:06:21 +0100

    next challenge started

---
+++ 7-reverse-interger.js
@@ -0,0 +1,37 @@
+// 7.Reverse Integer
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x
+// causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+// Example 1:
+// Input: x = 123
+// Output: 321
+
+// Example 2:
+// Input: 
+var x = -123;
+// Output: -321
+
+// Example 3:
+// Input: x = 120
+// Output: 21
+
+// Example 4:
+// Input: x = 0
+// Output: 0
+
+// Constraints:
+// -231 <= x <= 231 - 1
+
+var reverse = function (x) {
+  if (x === 0) return 0;
+
+  let answer = array();
+  
+
+};
+
+console.log(reverse(x));
+
+// Research: A signed integer is a 32-bit datum that encodes an integer in the range [-2147483648 to 2147483647].
